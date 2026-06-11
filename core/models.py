a7c5bcacd954c20d3d286c6c4e775aaa4b5d02d5
Admin <gguilhermemmaciel@gmail.com>
2026-06-11 10:39:01 -0300

    Atualização de Pedidos

--- core/models.py
+++ core/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 
 class Pedido(models.Model):
+
+    STATUS_CHOICES = [
+        ('ORCAMENTO', 'Orçamento'),
+        ('PRODUCAO', 'Produção'),
+        ('PRONTO', 'Pronto'),
+        ('ENTREGUE', 'Entregue'),
+    ]
+
     cliente = models.CharField(max_length=100)
-    produto = models.CharField(max_length=100)
-    quantidade = models.IntegerField()
-    data_pedido = models.DateTimeField(auto_now_add=True)
+    telefone = models.CharField(max_length=20)
+    servico = models.CharField(max_length=100)
+
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    sinal_pago = models.DecimalField(max_digits=10, decimal_places=2)
+
+    data_entrega = models.DateField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='ORCAMENTO'
+    )
+
+    arte_cliente = models.FileField(
+        upload_to='artes/',
+        blank=True,
+        null=True
+    )
+
+    criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.cliente} - {self.produto}"
+        return self.cliente
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
     path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
     path('pedidos/novo/', views.novo_pedido, name='novo_pedido'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
