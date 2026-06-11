fea7b0e4a2e04869381a9541885784194e58e79e
Admin <gguilhermemmaciel@gmail.com>
2026-06-11 09:56:48 -0300

    Atualização das views

--- core/views.py
+++ core/views.py
@@ -14,4 +14,17 @@ def lista_pedidos(request):
         {'pedidos': pedidos}
     )
 def novo_pedido(request):
+    if request.method == 'POST':
+        cliente = request.POST.get('cliente')
+        produto = request.POST.get('produto')
+        quantidade = request.POST.get('quantidade')
+
+        Pedido.objects.create(
+            cliente=cliente,
+            produto=produto,
+            quantidade=quantidade
+        )
+
+        return redirect('lista_pedidos')
+
     return render(request, 'pedidos/novo_pedido.html')
